3043d8e55ac09fe5e29a8428966a2fdb40ff5cfa
Barış Sakızlı <baris@bsakizli.com>
2020-01-15 15:54:23 +0300

    Add files via upload

---
+++ netasplanet.js
@@ -0,0 +1,41 @@
+
+$(document).ready(function(){
+
+var this_js_script = $('script[src*=netasplanet]'); // or better regexp to get the file name..
+var MagazineIdClient = this_js_script.attr('MagazineId');   
+
+
+		$.ajax({
+
+					type: "POST",
+					url: "http://10.112.11.43/api/counter/add",
+					headers: {
+						'Token': '9dc03930acd24d96e2b0584943c838b8'
+					},
+					contentType: "application/json; charset=utf-8",
+					data: JSON.stringify({
+						MagazineId: MagazineIdClient
+					}),
+					dataType: "json",
+
+					success: function(response) {
+						if (response.StatusCode == 200) {
+							//
+							console.log(response);
+							//
+						} else {
+						 console.log(response);
+						}
+					},
+
+					failure: function(response) {
+						console.log(response);
+
+					}
+
+				});
+
+
+});
+
+
